1b87e387cabdf66b46ca64794cf8d6111857a6be
Adeola Uthman <authman2@gmail.com>
2018-12-21 00:42:47 -0500

    Setup the view context menu

--- src/js/Work.js
+++ src/js/Work.js
@@ -41,6 +41,15 @@ var currentNote;
 var body;
 var pager;
 
+// The different contexts that are available.
+var vcm, secm, icm, stcm;
+const Contexts = {
+    View: {},
+    Selection: {},
+    Insert: {},
+    Settings: {}
+};
+var currentContext = Contexts.View;
 
 
 
@@ -55,17 +64,67 @@ const init = (root, pageManager) => {
     body = root;
     pager = pageManager;
 
-    Globals.loadHTMLInto('Work.html', root);
+    Globals.loadHTMLInto('Work.html', body);
+    
     setupRefs();
-
+    updateContextMenu();
 }
 
 /** Gets the references to all of the variables. */
 const setupRefs = () => {
-    
+    vcm = fs.readFileSync(`${__dirname}/../html/contexts/ViewContextMenu.html`, 'utf8');
+    secm = fs.readFileSync(`${__dirname}/../html/contexts/SelectionContextMenu.html`, 'utf8');
+    icm = fs.readFileSync(`${__dirname}/../html/contexts/InsertContextMenu.html`, 'utf8');
+    stcm = fs.readFileSync(`${__dirname}/../html/contexts/SettingsContextMenu.html`, 'utf8');
 }
 
+/** Updates the view depending on the type of context menu. */
+const updateContextMenu = () => {
+    $('.context-menu').remove();
+    
+    switch(currentContext) {
+        case Contexts.View:
+            body.innerHTML += vcm;
+            handleViewContextMenuActions();
+            break;
+        case Contexts.Selection:
+            body.innerHTML += secm;
+            break;
+        case Contexts.Insert:
+            body.innerHTML += icm;
+            break;
+        case Contexts.Settings:
+            body.innerHTML += stcm;
+            break;
+    }
+}
 
+/** Handles actions on the View Context Menu. */
+const handleViewContextMenuActions = () => {
+    const previousNoteButton = document.getElementById('vcm-previous-note-btn');
+    const nextNoteButton = document.getElementById('vcm-next-note-btn');
+    const newButton = document.getElementById('vcm-new-btn');
+    const saveButton = document.getElementById('vcm-save-btn');
+    const shareButton = document.getElementById('vcm-share-btn');
+    const notebooksButton = document.getElementById('vcm-notebooks-btn');
+    const notesButton = document.getElementById('vcm-notes-btn');
+
+    newButton.onclick = () => {
+        
+    }
+    saveButton.onclick = () => {
+
+    }
+    shareButton.onclick = () => {
+
+    }
+    notebooksButton.onclick = () => {
+
+    }
+    notesButton.onclick = () => {
+        
+    }
+}
 
 
 
