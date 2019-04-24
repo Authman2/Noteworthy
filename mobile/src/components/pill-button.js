7e327ae7aaff88eaa26a99290801a2c3164164b0
Adeola Uthman <authman2@gmail.com>
2019-04-24 14:33:33 -0400

    Get rid of the mobile folder, just make it responsive

--- mobile/src/components/pill-button.js
+++
@@ -1,5 +0,0 @@
-const Mosaic = require('@authman2/mosaic').default;
-
-module.exports = new Mosaic({
-    view: data => html`<button class='pill-button' onclick='${data.click}'>${ data.title }</button>`
-});
