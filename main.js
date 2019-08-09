e4b9971412bab4498692615f577bfb7b7c9d8d70
Adeola Uthman <authman2@gmail.com>
2019-08-09 17:54:17 -0400

    new version

--- main.js
+++ main.js
@@ -1,32 +1,79 @@
-const { app, BrowserWindow, Menu, MenuItem } = require('deskgap');
+const electron = require('electron');
+const path = require('path');
+const https = require('https');
 
-const fileMenu = new Menu();
-fileMenu.append(new MenuItem({
-    label: 'Save',
-    accelerator: 'Cmd+S',
-    click: (menuItem, wind) => {
-        console.log("Save!", menuItem);
-    }
-}))
+const { BrowserWindow, Menu } = electron;
+const app = electron.app;
 
-const menu = new Menu();
-menu.append(new MenuItem({
-    label: "File",
-    submenu: fileMenu
-}))
-
-// app.setMenu(menu);
-app.setName('Noteworthy');
-app.once('ready', () => {
-    const window = new BrowserWindow({
-        center: true,
-        closable: true,
-        frame: true,
-        icon: './res/NoteworthyLogo.png',
-        title: "Noteworthy",
-        titleBarStyle: 'hiddenInset',
-        menu: menu,
-        show: true
+const API_URL = 'https://noteworthy-backend.herokuapp.com';
+function fetchAppData(then) {
+    https.get(`${API_URL}/app-info`, res => {
+        let data = '';
+        res.on('data', chunk => {
+            data += chunk;
+        });
+        res.on('end', () => {
+            const d = JSON.parse(data);
+            then(d);
+        });
     });
-    window.loadURL('https://dev-noteworthyapp.netlify.com/login');
+}
+
+/************************
+*                       *
+*        METHODS        *
+*                       *
+*************************/
+
+/** Handles creating a new application window. */
+const createWindow = () => {
+    // Create a window object.
+    let wind = new BrowserWindow({
+        width: 775,
+        height: 600,
+        minWidth: 300,
+        minHeight: 300,
+        title: 'Noteworthy',
+        titleBarStyle: 'hiddenInset',
+        icon: path.join(__dirname, 'res/NoteworthyLogo2.png'),
+        webPreferences: {
+            nodeIntegration: true,
+        },
+        backgroundColor: 'white',
+    });
+
+    // Load the index.html file into the window.
+    wind.loadURL('https://noteworthyapp.netlify.com/login');
+}
+
+
+/************************
+*                       *
+*        STARTING       *
+*                       *
+*************************/
+
+let defaultOptions = {
+    applicationName: 'Noteworthy',
+    applicationVersion: '2.0.0',
+    copyright: 'Adeola Uthman 2015-2019',
+    credits: 'Adeola Uthman',
+    iconPath: path.join(__dirname, 'res/NoteworthyLogo2.png'),
+    version: '2.0.0',
+    website: 'https://adeolauthman.com/noteworthy'
+};
+app.setAboutPanelOptions(defaultOptions);
+app.setName('Noteworthy');
+app.dock.setIcon(path.join(__dirname, 'res/NoteworthyLogo.png'));
+
+app.on('ready', () => {
+    fetchAppData(data => {
+        app.setAboutPanelOptions({
+            ...defaultOptions,
+            ...data
+        });
+        createWindow();
+    });
 });
+app.on('window-all-closed', function () { if (process.platform !== 'darwin') app.quit() });
+app.on('activate', function () { if (BrowserWindow.getFocusedWindow() === null) createWindow() });
