9612ae14e3f814f7a3da69520908160e0f0a2e19
Adeola Uthman <authman2@gmail.com>
2019-08-08 23:12:47 -0400

    add topbar for dragging

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow, Menu, MenuItem } = require('deskgap');
+const path = require('path');
+
+const fileMenu = new Menu();
+fileMenu.append(new MenuItem({
+    label: 'Save',
+    accelerator: 'Cmd+S',
+    click: (menuItem, wind) => {
+        console.log("Save!", menuItem);
+    }
+}))
+
+const menu = new Menu();
+menu.append(new MenuItem({
+    label: "File",
+    submenu: fileMenu
+}))
+
+app.once('ready', () => {
+    const window = new BrowserWindow({
+        center: true,
+        closable: true,
+        frame: true,
+        icon: path.resolve('res', 'NoteworthyLogo.png'),
+        title: "Noteworthy",
+        titleBarStyle: 'hiddenInset',
+        menu: menu,
+        show: true
+    });
+    window.loadURL('https://noteworthyapp.netlify.com/login');
+});
