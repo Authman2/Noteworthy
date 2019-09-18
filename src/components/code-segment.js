207d4e415d798b140beb22451de5983e499d0e11
Adeola Uthman <authman2@gmail.com>
2019-09-18 13:26:27 -0400

    You can fix code segments later

--- src/components/code-segment.js
+++ src/components/code-segment.js
@@ -1,17 +1,20 @@
 import Mosaic from 'mosaic-framework';
 import hljs from 'highlight.js';
+import _ from 'underscore';
 
 export default new Mosaic({
     name: 'code-segment',
-    data: {
-        language: 'javascript'
-    },
     created() {
-        const node = this;
-        hljs.highlightBlock(node);
+        this.hl();
 
         this.addEventListener('mouseover', this.disableEdits);
         this.addEventListener('mouseleave', this.enableEdits);
+        // window.addEventListener('input', e => {
+        //     if(!e.target || !e.target.classList.contains('code-segment'))
+        //         return;
+
+        //     _.debounce(this.hl.bind(this), 4000)();
+        // })
     },
     disableEdits() {
         const content = document.getElementById('note-field');
@@ -21,29 +24,13 @@ export default new Mosaic({
         const content = document.getElementById('note-field');
         content.contentEditable = true;
     },
-    toggleDropdown() {
-        const dropdown = document.getElementById("myDropdown");
-        console.log(dropdown);
-        //.classList.toggle("show");
-    },
-    selectLanguage(lang) {
-        this.data.language = lang;
-        document.getElementById("myDropdown").classList.toggle("show");
-        console.log('Got here');
+    hl() {
+        hljs.highlightBlock(this.firstChild);
+        console.log('Now highlighting!', this);
     },
     view() {
         return html`
-        <section class="dropdown">
-            <button onclick="${this.toggleDropdown}" class="code-segment-select-button">
-                ${this.data.language}
-            </button>
-            <div id="myDropdown" class="dropdown-content">
-                <option onclick='${this.selectLanguage.bind(this, 'javascript')}'>JavaScript</option>
-                <option onclick='${this.selectLanguage.bind(this, 'typescript')}'>Typescript</option>
-                <option onclick='${this.selectLanguage.bind(this, 'cpp')}'>C++</option>
-            </div>
-        </section>
-        
-        <div class='code-segment' contenteditable="true">var x = 5;</div>`
+        <div class='code-segment' contenteditable="true">var x = 5;</div>
+        `
     }
 })
